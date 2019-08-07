fad4cc1b6228d61bde2358fa75f2c2eea44fd20f
yunmoon <meiyun@zhiyuanhui.com>
2019-08-07 10:06:06 +0800

    update logger format

--- dist/decorators/logger.js
+++ dist/decorators/logger.js
@@ -1,8 +1,19 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const winston = require("winston");
+const moment = require("moment");
 const appDir = process.cwd();
+const { combine, timestamp, label, printf } = winston.format;
+const myFormat = printf(({ level, message, label, timestamp }) => {
+    return JSON.stringify({
+        level,
+        data: message,
+        appName: label,
+        date: moment(timestamp).format("YYYY-MM-DD HH:mm:ss.SSS")
+    });
+});
 const winstonLogger = winston.createLogger({
+    format: combine(label({ label: process.env.APP_NAME || "" }), timestamp(), myFormat),
     transports: [
         new winston.transports.Console(),
         new winston.transports.File({
--- src/decorators/logger.ts
+++ src/decorators/logger.ts
@@ -1,6 +1,21 @@
 import * as winston from "winston";
+import * as moment from "moment";
 const appDir = process.cwd();
+const { combine, timestamp, label, printf } = winston.format;
+const myFormat = printf(({ level, message, label, timestamp }) => {
+  return JSON.stringify({
+    level,
+    data: message,
+    appName: label,
+    date: moment(timestamp).format("YYYY-MM-DD HH:mm:ss.SSS")
+  });
+});
 const winstonLogger = winston.createLogger({
+  format: combine(
+    label({ label: process.env.APP_NAME || "" }),
+    timestamp(),
+    myFormat
+  ),
   transports: [
     new winston.transports.Console(),
     new winston.transports.File({
